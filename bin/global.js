ce5f1748dd906dab2b1aafe20ce536d045585c87
Patricio <isc.patricio@gmail.com>
2023-06-05 16:44:15 -0600

    fix error in global file!

--- bin/global.js
+++ bin/global.js
@@ -1,2 +1,4 @@
+#!/usr/bin/env node
 let random = require("../src/index.js");
+
 random.funnyCommit();
--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ const messages = [
 ];
 
 const funnyCommit = () => {
-  const message = messages[Math.floor(Math.random()) * messages.length];
+  const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
 };
-module.exports = { funnyCommit };
+
+module.exports = {
+  funnyCommit,
+};
